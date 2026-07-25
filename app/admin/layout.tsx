9244a666d619470be209cca8aca7afd15e6d97c0
AmineBouzid007 <stikky.stickers.shop@gmail.com>
2026-07-25 02:08:34 +0100

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,9 @@
+import React from "react";
+
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div>
-      {children}
-    </div>
-  );
+  return <>{children}</>;
 }
