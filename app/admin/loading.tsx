cf812570bad52574809860b096b343f160171546
AmineBouzid007 <stikky.stickers.shop@gmail.com>
2026-07-25 01:30:48 +0100

    Update loading.tsx

--- app/admin/loading.tsx
+++ app/admin/loading.tsx
@@ -1,8 +1,3 @@
-
 export default function Loading() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      Loading...
-    </div>
-  );
+  return <div>Loading...</div>;
 }
